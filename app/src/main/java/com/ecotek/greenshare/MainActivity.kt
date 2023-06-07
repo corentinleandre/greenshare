fef3a4f122c007d2c3ac4ace7983a99682ccb352
Capet2013 <hugues.gf59@gmail.com>
2023-06-07 11:05:49 +0200

    Permet de changer de page lorsqu'on clique sur la barre de navigation

--- app/src/main/java/com/ecotek/greenshare/MainActivity.kt
+++ app/src/main/java/com/ecotek/greenshare/MainActivity.kt
@@ -3,18 +3,24 @@ package com.ecotek.greenshare
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import androidx.core.view.WindowCompat
 import android.view.Menu
 import android.view.MenuItem
+import androidx.fragment.app.Fragment
 import com.ecotek.greenshare.databinding.ActivityMainBinding
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+
+    private lateinit var bottomNavigationView: BottomNavigationView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         WindowCompat.setDecorFitsSystemWindows(window, false)
@@ -23,9 +29,34 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        setSupportActionBar(binding.toolbar)
+        //setSupportActionBar(binding.toolbar)
 
-        val database = FirebaseDatabase.getInstance()
+        bottomNavigationView = findViewById(R.id.bottom_nav);
+        bottomNavigationView.setOnNavigationItemSelectedListener { menuItem ->
+            when(menuItem.itemId){
+                R.id.nav_home -> {
+                    moveToFragment(HomeFragment())
+                    return@setOnNavigationItemSelectedListener true
+                }
+                R.id.nav_New_content -> {
+                    moveToFragment(NewContentFragment())
+                    return@setOnNavigationItemSelectedListener true
+                }
+                R.id.nav_profile -> {
+                    moveToFragment(ProfileFragment())
+                    return@setOnNavigationItemSelectedListener true
+                }
+                R.id.nav_search -> {
+                    moveToFragment(SearchFragment())
+                    return@setOnNavigationItemSelectedListener true
+                }
+            }
+            false
+        }
+
+
+
+        /*val database = FirebaseDatabase.getInstance()
         val myRef = database.getReference("message")
 
         binding.fab.setOnClickListener { view ->
@@ -56,9 +87,9 @@ class MainActivity : AppCompatActivity() {
         // Inflate the menu; this adds items to the action bar if it is present.
         menuInflater.inflate(R.menu.menu_main, menu)
         return true
-    }
+    }*/
 
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+ /*   override fun onOptionsItemSelected(item: MenuItem): Boolean {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
@@ -66,5 +97,12 @@ class MainActivity : AppCompatActivity() {
             R.id.action_settings -> true
             else -> super.onOptionsItemSelected(item)
         }
+    }*/
+
+        }
+
+    private fun moveToFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction().replace(R.id.container_view, fragment).commit()
     }
+
 }
