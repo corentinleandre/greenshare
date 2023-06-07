d2b792f846a7478978e527e21609d8cccb21b72a
Théo Langevin <theolangevin3@gmail.com>
2023-06-07 17:35:20 +0200

    Add home fragment and multiples xml

--- app/src/main/java/com/ecotek/greenshare/HomeFragment.kt
+++ app/src/main/java/com/ecotek/greenshare/HomeFragment.kt
@@ -1,59 +1,70 @@
-package com.ecotek.greenshare
+package com.example.applicationtest2
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.LinearLayout
+import android.widget.ImageView
+import android.app.Activity
+import android.widget.ScrollView
+import androidx.fragment.app.Fragment
+import com.ecotek.greenshare.R
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [HomeFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class HomeFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    val im = 10 // Nombre d'ViewImage et ViewText à ajouter
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+    var image = mutableListOf<String>("a", "ameen", "r", "a", "ameen", "r")
+    var texte = mutableListOf<String>("a", "ameen", "r")
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val view = inflater.inflate(R.layout.fragment_home, container, false)
+        CreatePost(view)
+        val scrollView: ScrollView = view.findViewById(R.id.scroll)
+        detectEndOfScroll(scrollView)
+        refreshScroll(scrollView)
+        return view
+    }
+
+    private fun CreatePost(view: View) {
+        val linearContainer: LinearLayout = view.findViewById(R.id.fil)
+        for (index in 0 until 9) {
+            val inflater = LayoutInflater.from(requireContext())
+            val postView = inflater.inflate(R.layout.post, null)
+            linearContainer.addView(postView)
+
+            if (image.getOrNull(index) != null) {
+                val imageView: ImageView = postView.findViewById(R.id.imageView)
+                val imageName = image[index]
+                val imageId = resources.getIdentifier(imageName, "drawable", requireActivity().packageName)
+                imageView.setImageResource(imageId)
+            }
+            if (texte.getOrNull(index) != null) {
+                val textView: TextView = postView.findViewById(R.id.textView)
+                val textValue = texte[index]
+                textView.text = textValue
+            }
         }
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+    private fun detectEndOfScroll(scrollView: ScrollView) {
+        scrollView.viewTreeObserver.addOnScrollChangedListener {
+            val view = scrollView.getChildAt(scrollView.childCount - 1)
+            val diff = (view.bottom - (scrollView.height + scrollView.scrollY))
+            if (diff == 0) {
+                CreatePost(view)
+            }
+        }
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment HomeFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            HomeFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
+    private fun refreshScroll(scrollView: ScrollView) {
+        scrollView.viewTreeObserver.addOnScrollChangedListener {
+            if (scrollView.scrollY == 0) {
+                CreatePost(scrollView)
             }
+        }
     }
 }
