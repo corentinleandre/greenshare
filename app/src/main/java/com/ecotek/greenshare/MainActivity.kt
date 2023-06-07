f818d9a53895fc017000f250115e6031ae0d32e8
Capet2013 <hugues.gf59@gmail.com>
2023-06-07 15:34:27 +0200

    Nettoyage du MainActivity

--- app/src/main/java/com/ecotek/greenshare/MainActivity.kt
+++ app/src/main/java/com/ecotek/greenshare/MainActivity.kt
@@ -1,18 +1,10 @@
 package com.ecotek.greenshare
 
 import android.os.Bundle
-import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.widget.Toolbar
 import androidx.core.view.WindowCompat
-import android.view.Menu
-import android.view.MenuItem
 import androidx.fragment.app.Fragment
 import com.ecotek.greenshare.databinding.ActivityMainBinding
-import com.google.firebase.database.DataSnapshot
-import com.google.firebase.database.DatabaseError
-import com.google.firebase.database.FirebaseDatabase
-import com.google.firebase.database.ValueEventListener
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 
@@ -29,9 +21,7 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        //setSupportActionBar(binding.toolbar)
-
-        bottomNavigationView = findViewById(R.id.bottom_nav);
+        bottomNavigationView = findViewById(R.id.bottom_nav)
         moveToFragment(HomeFragment())
         bottomNavigationView.setOnItemSelectedListener { menuItem ->
             when(menuItem.itemId){
@@ -54,53 +44,7 @@ class MainActivity : AppCompatActivity() {
             }
             false
         }
-
-
-
-        /*val database = FirebaseDatabase.getInstance()
-        val myRef = database.getReference("message")
-
-        binding.fab.setOnClickListener { view ->
-
-
-            myRef.setValue("Hello, World!")
-
-
-        }
-
-        myRef.addValueEventListener(object: ValueEventListener {
-
-            override fun onDataChange(snapshot: DataSnapshot) {
-                // This method is called once with the initial value and again
-                // whenever data at this location is updated.
-                val value = snapshot.getValue()
-                Log.d("MainActivity", "Value is: " + value)
-            }
-
-            override fun onCancelled(error: DatabaseError) {
-                Log.w("MainActivity", "Failed to read value.", error.toException())
-            }
-
-        })
     }
-
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return true
-    }*/
-
- /*   override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        return when (item.itemId) {
-            R.id.action_settings -> true
-            else -> super.onOptionsItemSelected(item)
-        }
-    }*/
-
-        }
 
     private fun moveToFragment(fragment: Fragment) {
         supportFragmentManager.beginTransaction().replace(R.id.container_view, fragment).commit()
