c649078e55a61fe390394dd35a64fb6365cda9ae
ameen <nurhaqeemameen@gmail.com>
2023-06-09 00:10:55 +0200

    commented useless function

--- app/src/main/java/com/ecotek/greenshare/LoginActivity.kt
+++ app/src/main/java/com/ecotek/greenshare/LoginActivity.kt
@@ -20,11 +20,11 @@ open class LoginActivity : AppCompatActivity() {
         setContentView(R.layout.login_activity)
         //FireStore().getJsonDataFromAsset(this, "prof_data.json")
         //FireStore().getJsonDataFromAsset(this, "student_data.json")
-        Article.getArticle("1"){article ->
-            if (article != null){
-                Toast.makeText(this@LoginActivity, article.content, Toast.LENGTH_LONG).show()
-            }
-        }
+        //Article.getArticle("1"){article ->
+        //    if (article != null){
+        //        Toast.makeText(this@LoginActivity, article.content, Toast.LENGTH_LONG).show()
+        //    }
+        //}
 
         var loginbtn = findViewById<Button>(R.id.loginbtn)
         var mail = findViewById<EditText>(R.id.email)
