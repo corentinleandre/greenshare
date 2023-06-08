812929565791de1074b79eceae0bc3f85d6bc610
Théo Langevin <theolangevin3@gmail.com>
2023-06-08 17:08:00 +0200

    Local version of fil linked with posts : display post when clicked

--- app/src/main/java/com/ecotek/greenshare/HomeFragment.kt
+++ app/src/main/java/com/ecotek/greenshare/HomeFragment.kt
@@ -1,21 +1,25 @@
 package com.ecotek.greenshare
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import android.widget.LinearLayout
 import android.widget.ImageView
+import android.widget.RelativeLayout
 import android.widget.ScrollView
+import androidx.cardview.widget.CardView
 import androidx.fragment.app.Fragment
 
 
 class HomeFragment : Fragment() {
     val im = 10 // Nombre d'ViewImage et ViewText à ajouter
 
-    var image = mutableListOf<String>("a", "ameen", "r", "a", "ameen", "r")
-    var texte = mutableListOf<String>("a", "ameen", "r")
+    var image = mutableListOf<String>("a", "ameen", "r", "a", "ameen", "r","a", "ameen", "r", "a", "ameen", "r")
+    var texte = mutableListOf<String>("a", "ameen", "r", "a", "ameen", "r","a", "ameen", "r", "a", "ameen", "r")
+    var description = mutableListOf<String>("orem ipsum dolor sit amet, consectetur adipiscing elit. Proin sagittis, velit ut sollicitudin facilisis, diam justo elementum mi, ut consectetur neque leo in quam. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nullam consectetur commodo nisi vel semper. Etiam dapibus erat odio, et placerat elit maximus at. Pellentesque finibus erat a mi tempus, accumsan lobortis orci ultrices. Aenean dolor sem, varius sit amet aliquam id, pellentesque eget massa. Mauris accumsan mi et volutpat pretium. Aliquam dictum mattis massa vel bibendum. Mauris tempus augue eget est rhoncus, ac maximus nunc fermentum.", "Etiam nec neque turpis. Donec nec aliquam sapien. Nulla vehicula rutrum neque, in blandit velit placerat vitae. Suspendisse volutpat blandit sem, at consequat quam imperdiet sit amet. Curabitur maximus, urna vel fringilla volutpat, lectus mauris tincidunt dui, quis rhoncus diam ipsum in erat. Sed ex orci, tempor sit amet fermentum eu, blandit eget nisi. Nunc elit magna, tristique rhoncus egestas quis, lacinia non lacus. Sed dapibus risus a mauris varius sodales. Integer sed faucibus ipsum.", "Morbi aliquet mollis velit, a venenatis nunc semper at. Fusce et faucibus ipsum, a placerat lacus. Nulla placerat mattis risus in consectetur. Cras sit amet nibh ut lorem ullamcorper porttitor sed sed turpis. Cras mi dolor, tincidunt eu vestibulum id, condimentum et tellus. Fusce enim nisi, rutrum ut diam vel, elementum tristique massa. Aenean sodales efficitur risus, sit amet gravida nisi iaculis et. Sed maximus mauris a felis ultricies accumsan. Pellentesque vel turpis laoreet, gravida est in, porttitor massa. Phasellus ut urna sit amet tellus tristique eleifend non quis velit.", "Morbi aliquet mollis velit, a venenatis nunc semper at. Fusce et faucibus ipsum, a placerat lacus. Nulla placerat mattis risus in consectetur. Cras sit amet nibh ut lorem ullamcorper porttitor sed sed turpis. Cras mi dolor, tincidunt eu vestibulum id, condimentum et tellus. Fusce enim nisi, rutrum ut diam vel, elementum tristique massa. Aenean sodales efficitur risus, sit amet gravida nisi iaculis et. Sed maximus mauris a felis ultricies accumsan. Pellentesque vel turpis laoreet, gravida est in, porttitor massa. Phasellus ut urna sit amet tellus tristique eleifend non quis velit.", "Morbi aliquet mollis velit, a venenatis nunc semper at. Fusce et faucibus ipsum, a placerat lacus. Nulla placerat mattis risus in consectetur. Cras sit amet nibh ut lorem ullamcorper porttitor sed sed turpis. Cras mi dolor, tincidunt eu vestibulum id, condimentum et tellus. Fusce enim nisi, rutrum ut diam vel, elementum tristique massa. Aenean sodales efficitur risus, sit amet gravida nisi iaculis et. Sed maximus mauris a felis ultricies accumsan. Pellentesque vel turpis laoreet, gravida est in, porttitor massa. Phasellus ut urna sit amet tellus tristique eleifend non quis velit.", "Morbi aliquet mollis velit, a venenatis nunc semper at. Fusce et faucibus ipsum, a placerat lacus. Nulla placerat mattis risus in consectetur. Cras sit amet nibh ut lorem ullamcorper porttitor sed sed turpis. Cras mi dolor, tincidunt eu vestibulum id, condimentum et tellus. Fusce enim nisi, rutrum ut diam vel, elementum tristique massa. Aenean sodales efficitur risus, sit amet gravida nisi iaculis et. Sed maximus mauris a felis ultricies accumsan. Pellentesque vel turpis laoreet, gravida est in, porttitor massa. Phasellus ut urna sit amet tellus tristique eleifend non quis velit.")
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.fragment_home, container, false)
@@ -27,7 +31,6 @@ class HomeFragment : Fragment() {
     }
 
     private fun createPost(view: View) {
-
         if (!isAdded) {
             return  // Vérifie si le fragment est attaché avant d'accéder au contexte
         }
@@ -36,25 +39,53 @@ class HomeFragment : Fragment() {
         for (index in 0 until im) {
             val inflater = LayoutInflater.from(requireContext())
             val postView = inflater.inflate(R.layout.post, null)
-            linearContainer.addView(postView)
+            val cardView: CardView = postView.findViewById(R.id.touchCard)
 
+
+            linearContainer.addView(postView)
+            val args = Bundle()
+            //val cardView: CardView = view.findViewById(R.id.touchCard)
+            //cardView.setOnClickListener {}
+
+
+            //Rajouter une margin TOP de 40dp pour le tout premier post
+            if (index == 0) {
+                val layoutParams = postView.layoutParams as ViewGroup.MarginLayoutParams
+                val marginTop = resources.getDimensionPixelSize(R.dimen.margin_top) // Remplace R.dimen.margin_top par la ressource correspondante à 20dp
+                layoutParams.setMargins(layoutParams.leftMargin, marginTop, layoutParams.rightMargin, layoutParams.bottomMargin)
+                postView.layoutParams = layoutParams
+            }
             if (image.getOrNull(index) != null) {
+
                 val imageView: ImageView = postView.findViewById(R.id.imageView)
                 val imageName = image[index]
                 val imageId = resources.getIdentifier(imageName, "drawable", requireActivity().packageName)
                 imageView.setImageResource(imageId)
-                imageView.setOnClickListener {
-                    handleClick()
-                }
             }
             if (texte.getOrNull(index) != null) {
                 val textView: TextView = postView.findViewById(R.id.textView)
-                val textValue = texte[index]
+                var textValue = texte[index]
                 textView.text = textValue
-                textView.setOnClickListener {
-                    handleClick()
-                }
             }
+            cardView.setOnClickListener {
+                val textView: TextView = postView.findViewById(R.id.textView)
+                var textValue = texte[index]
+                textView.text = textValue
+                val imageView: ImageView = postView.findViewById(R.id.imageView)
+                val imageName = image[index]
+                val imageId = resources.getIdentifier(imageName, "drawable", requireActivity().packageName)
+                imageView.setImageResource(imageId)
+                var descript=description[index]
+
+                args.putString("keyd",descript)
+                args.putString("keyi", imageName)
+                args.putString("key", textValue)
+                val readFragment = ReadFragment()
+                readFragment.arguments = args
+                handleClick(readFragment, args)
+            }
+
+
         }
     }
 
@@ -77,11 +108,12 @@ class HomeFragment : Fragment() {
             }
         }
     }
-    private fun handleClick() {
-        println("Post")
-        (activity as HomeActivity).moveToFragment(NewContentFragment())
+    fun handleClick(fragment:Fragment, arguments: Bundle) {
+        fragment.arguments = arguments
+        (activity as HomeActivity).moveToFragment(fragment)
 
     }
+
 
 
 }
