5e0ac79b6cd13e4f3459f12b81edf2b566345d5e
ameen <nurhaqeemameen@gmail.com>
2023-06-09 11:26:31 +0200

    added photo functionality

--- app/src/main/java/com/ecotek/greenshare/AddPostFragment.kt
+++ app/src/main/java/com/ecotek/greenshare/AddPostFragment.kt
@@ -14,6 +14,17 @@ import com.google.firebase.firestore.FirebaseFirestore
 import java.text.SimpleDateFormat
 import java.util.Date
 import java.util.Locale
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
+import android.provider.MediaStore
+import android.widget.ImageButton
+import android.widget.ImageView
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 
 //private const val ARG_PARAM1 = "param1"
 //private const val ARG_PARAM2 = "param2"
@@ -34,6 +45,13 @@ class AddPostFragment : Fragment() {
         val titleAreaTextInput = view.findViewById<TextInputEditText>(R.id.titleArea)
         val contentAreaTextInput = view.findViewById<TextInputEditText>(R.id.contentArea)
         val postButton = view.findViewById<Button>(R.id.postbtn)
+
+        val imageButton = view.findViewById<ImageButton>(R.id.imgbtn)
+        val imageView = view.findViewById<ImageView>(R.id.imageView4)
+
+        imageButton.setOnClickListener {
+            checkPermissionAndOpenGallery()
+        }
 
         postButton.setOnClickListener {
             val currentUser = FirebaseAuth.getInstance().currentUser
@@ -57,6 +75,61 @@ class AddPostFragment : Fragment() {
                 }
 
         }
+    }
+
+    private val PERMISSION_REQUEST_CODE = 2
+
+    private lateinit var imageButton: ImageButton
+    private lateinit var imageView: ImageView
+
+    private val pickImageLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+        if (result.resultCode == AppCompatActivity.RESULT_OK) {
+            val data: Intent? = result.data
+            val pickedPhoto: Uri? = data?.data
+            if (pickedPhoto != null) {
+                imageView.setImageURI(pickedPhoto)
+            }
+        }
+    }
+
+    private fun checkPermissionAndOpenGallery() {
+        if (ContextCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.READ_EXTERNAL_STORAGE
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            requestPermission()
+        } else {
+            openGallery()
+        }
+    }
+
+    private fun requestPermission() {
+        ActivityCompat.requestPermissions(
+            requireActivity(),
+            arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),
+            PERMISSION_REQUEST_CODE
+        )
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == PERMISSION_REQUEST_CODE) {
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                openGallery()
+            } else {
+                // Handle the case where the permission is denied
+            }
+        }
+    }
+
+    private fun openGallery() {
+        val intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+        pickImageLauncher.launch(intent)
     }
 
     companion object {
