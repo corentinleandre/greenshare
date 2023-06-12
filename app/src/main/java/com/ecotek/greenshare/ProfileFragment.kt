23598bac539a6a7fdc463a30c491bdbf2e992bf9
ameen <nurhaqeemameen@gmail.com>
2023-06-12 15:08:40 +0200

    REadded logout button

--- app/src/main/java/com/ecotek/greenshare/ProfileFragment.kt
+++ app/src/main/java/com/ecotek/greenshare/ProfileFragment.kt
@@ -1,5 +1,6 @@
 package com.ecotek.greenshare
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
@@ -7,6 +8,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.ImageButton
 import android.widget.TextView
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
@@ -40,6 +42,13 @@ class ProfileFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_profile, container, false)
+        val logoutButton: ImageButton = view.findViewById(R.id.logoutButton)
+
+        logoutButton.setOnClickListener{
+            FirebaseAuth.getInstance().signOut()
+            startActivity(Intent((activity as HomeActivity),LoginActivity::class.java))
+            this.activity?.finish()
+        }
         //add the user's personal data
         val currentUser = FirebaseAuth.getInstance().currentUser?.email
         val mFirestore = FirebaseFirestore.getInstance()
