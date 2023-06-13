4af348888b41da2033553893181a5c55247a4c2c
Yahya4elaili <135591455+Yahya4elaili@users.noreply.github.com>
2023-06-13 20:11:07 +0200

    implemented the posts in the profile

--- app/src/main/java/com/ecotek/greenshare/ProfileFragment.kt
+++ app/src/main/java/com/ecotek/greenshare/ProfileFragment.kt
@@ -1,5 +1,6 @@
 package com.ecotek.greenshare
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
@@ -7,9 +8,15 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.ScrollView
 import android.widget.TextView
+import com.bumptech.glide.Glide
+import androidx.cardview.widget.CardView
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.Query
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -25,7 +32,7 @@ class ProfileFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
-
+    var currentId=0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
@@ -41,29 +48,205 @@ class ProfileFragment : Fragment() {
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_profile, container, false)
         //add the user's personal data
-        val currentUser = FirebaseAuth.getInstance().currentUser?.email
+        val currentUser = FirebaseAuth.getInstance().currentUser
+        val currentUserEmail = currentUser?.email
+        var currentUserID:String = ""
         val mFirestore = FirebaseFirestore.getInstance()
 
         mFirestore.collection("Users")
-            .whereEqualTo("email", currentUser)
+            .whereEqualTo("email", currentUserEmail)
             .get()
             .addOnSuccessListener { document ->
                 val userDocument = document.documents[0]
                 val phoneNumber = userDocument.getString("telephone")
                 val roleUser = userDocument.getString("role")
                 val groupUser = userDocument.getString("group")
+                currentUserID = userDocument.getString("identification").toString()
                 var list = userDocument.getString("email")?.split(".", "@")
                 var firstname = list?.get(0)?.capitalize()
                 var lastname = list?.get(1)?.capitalize()
-                view.findViewById<TextView>(R.id.user_name).text = "$firstname $lastname"
+              /*  view.findViewById<TextView>(R.id.user_name).text = "$firstname $lastname"
                 view.findViewById<TextView>(R.id.user_role).text = roleUser
                 view.findViewById<TextView>(R.id.user_group).text = groupUser
                 view.findViewById<TextView>(R.id.user_numero).text = phoneNumber
-                view.findViewById<TextView>(R.id.user_email).text = currentUser
-                view.findViewById<TextView>(R.id.user_bureau).text = "Plus tard" //TODO : add to data base "bureau"
+                view.findViewById<TextView>(R.id.user_email).text = currentUserEmail
+                view.findViewById<TextView>(R.id.user_bureau).text = "Plus tard"*/ //TODO : add to data base "bureau"
+
+                Log.d("ProfileFragment","Current user :" + currentUserID)
+                createPost(view,currentUserID)
+                val scrollView: ScrollView = view.findViewById(R.id.scroller)
+                detectEndOfScroll(scrollView, currentUserID)
+                refreshScroll(scrollView, currentUserID)
+
+            }
+        return view
+    }
+    @SuppressLint("MissingInflatedId")
+    private fun createPost(view: View,currentUserID:String?){
+        Log.d("ProfileFragment", "createPost called with currentUserID: $currentUserID")
+        if (!isAdded) {
+            return  // Vérifie si le fragment est attaché avant d'accéder au contexte
+        }
+
+        val linearContainer: LinearLayout = view.findViewById(R.id.scroller)
+
+        val mFirestore = FirebaseFirestore.getInstance()
+        val collection = mFirestore.collection("Article")
+        // Tri par ordre décroissant des IDs
+        collection
+            .whereEqualTo("authorID", currentUserID)
+            .orderBy("date", Query.Direction.DESCENDING).limit(8)
+            .get()
+            .addOnSuccessListener { querySnapshot ->
+                val articles = ArrayList<Article>()
+                for (document in querySnapshot) {
+                    val id = document.id.toString()
+                    Article.getArticle(id) { article ->
+                        if (article != null) {
+                            articles.add(article)
+                        }
+                        // Vérifie si tous les articles ont été récupérés
+                        if (articles.size == querySnapshot.documents.size) {
+                            articles.sortByDescending { it.date } // Trie les articles par ordre décroissant de la date
+                            for (article in articles) {
+                                val inflater = LayoutInflater.from(requireContext())
+                                val postView = inflater.inflate(R.layout.post, null)
+                                val cardView: CardView = postView.findViewById(R.id.touchCard)
+                                linearContainer.addView(postView)
+
+                                val args = Bundle()
+
+                                if (article.mediasID != "") {
+                                    val medias = FirebaseFirestore.getInstance().collection("Medias").document(article.id)
+                                    medias.get().addOnSuccessListener { documentSnapshot ->
+                                        val media1 = documentSnapshot.getString("media1")
+                                        val mediaView: ImageView = postView.findViewById(R.id.imageView)
+                                        Glide.with(requireContext())
+                                            .load(media1)
+                                            .into(mediaView)
+                                    }
+                                }
+
+                                val textView: TextView = postView.findViewById(R.id.textView)
+                                textView.text = article.title
+
+                                cardView.setOnClickListener {
+                                    args.putString("index", article.id)
+                                    val readFragment = ReadFragment()
+                                    readFragment.arguments = args
+                                    handleClick(readFragment, args)
+                                }
+                                currentId = articles.lastOrNull()?.id?.toInt() ?: 0
+                            }
+                        }
+                    }
+                }
+            }
+            .addOnFailureListener { exception ->
+                //
             }
 
-        return view
+    }
+
+    private fun createnextPost(view: View, currentUserID: String?) {
+        Log.d("ProfileFragment", "Current user 2 :$currentUserID")
+
+        if (!isAdded) {
+            return  // Vérifie si le fragment est attaché avant d'accéder au contexte
+        }
+
+        val linearContainer: LinearLayout = view.findViewById(R.id.scroller)
+
+        val mFirestore = FirebaseFirestore.getInstance()
+        val collection = mFirestore.collection("Article")
+        collection
+            .whereEqualTo("authorID", currentUserID)
+            .get()
+            .addOnSuccessListener { querySnapshot ->
+                val articles = ArrayList<Article>()
+                for (document in querySnapshot) {
+                    val id = document.id.toString()
+                    Article.getArticle(id) { article ->
+                        if (article != null) {
+                            articles.add(article)
+                        }
+                        // Vérifie si tous les articles ont été récupérés
+                        if (articles.size == querySnapshot.documents.size) {
+                            articles.sortByDescending { it.date } // Trie les articles par ordre décroissant de la date
+                            val smallestId = articles.lastOrNull()?.id?.toInt() ?: 0
+                            val idLimit = currentId - 5
+                            val articlesToDisplay = articles.filter { it.id.toInt() < currentId && it.id.toInt() > idLimit }
+                            for (article in articlesToDisplay) {
+                                val inflater = LayoutInflater.from(requireContext())
+                                val postView = inflater.inflate(R.layout.post, null)
+                                val cardView: CardView = postView.findViewById(R.id.touchCard)
+                                linearContainer.addView(postView)
+
+                                val args = Bundle()
+
+                                if (article.mediasID != "") {
+                                    val medias = FirebaseFirestore.getInstance().collection("Medias").document(article.id)
+                                    medias.get().addOnSuccessListener { documentSnapshot ->
+                                        val media1 = documentSnapshot.getString("media1")
+                                        val mediaView: ImageView = postView.findViewById(R.id.imageView)
+                                        Glide.with(requireContext())
+                                            .load(media1)
+                                            .into(mediaView)
+                                    }
+                                }
+
+                                val textView: TextView = postView.findViewById(R.id.textView)
+                                textView.text = article.title
+
+                                cardView.setOnClickListener {
+                                    args.putString("index", article.id)
+                                    val readFragment = ReadFragment()
+                                    readFragment.arguments = args
+                                    handleClick(readFragment, args)
+                                }
+                            }
+                            currentId = articlesToDisplay.lastOrNull()?.id?.toInt() ?: 0
+                        }
+                    }
+                }
+            }
+            .addOnFailureListener { exception ->
+                //
+            }
+    }
+
+
+
+
+
+    private fun detectEndOfScroll(scrollView: ScrollView, currentUserID: String?) {
+        Log.d("ProfileFragment", "detectEndOfScroll called with currentUserID: $currentUserID")
+        scrollView.viewTreeObserver.addOnScrollChangedListener {
+            val view = scrollView.getChildAt(scrollView.childCount - 1)
+            val diff = (view.bottom - (scrollView.height + scrollView.scrollY))
+            if (diff == 0) {
+                //changer la fonction create post par la fonction add other posts qui ajoute les posts des id suivants
+                createnextPost(view, currentUserID)
+            }
+        }
+    }
+
+    private fun refreshScroll(scrollView: ScrollView, currentUserID: String?) {
+        Log.d("ProfileFragment", "refreshScroll called with currentUserID: $currentUserID")
+        scrollView.viewTreeObserver.addOnScrollChangedListener {
+            if (scrollView.scrollY == 0) {
+                currentId=0
+
+                createPost(scrollView.getChildAt(0),currentUserID)
+
+            }
+        }
+    }
+
+    fun handleClick(fragment: Fragment, arguments: Bundle) {
+        fragment.arguments = arguments
+        (activity as HomeActivity).moveToFragment(fragment)
+
     }
 
     companion object {
