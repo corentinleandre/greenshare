bb7ec1fff52194377e2ac8274cde613115c23f1b
ameen <nurhaqeemameen@gmail.com>
2023-06-14 13:30:39 +0200

    fixed bug

--- app/src/main/java/com/ecotek/greenshare/HomeFragment.kt
+++ app/src/main/java/com/ecotek/greenshare/HomeFragment.kt
@@ -24,28 +24,26 @@ import com.google.firebase.firestore.FieldPath
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.Query
 
+
 class HomeFragment : Fragment() {
-    var currentId = 0
-    var userRights = "0"
+    var currentId=0
+    var userRights="0"
     val currentUser = FirebaseAuth.getInstance().currentUser?.email
 
     private lateinit var progressBar: ProgressBar
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.fragment_home, container, false)
+        createPost(view)
+        val scrollView: ScrollView = view.findViewById(R.id.scroll)
+        detectEndOfScroll(scrollView)
+        refreshScroll(scrollView)
+
         return view
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        val view = view
-        if (view != null) {
-            createPost(view)
-        }
-    }
-
     @SuppressLint("MissingInflatedId")
-    private fun createPost(view: View) {
+    private fun createPost(view: View){
         if (!isAdded) {
             return  // Vérifie si le fragment est attaché avant d'accéder au contexte
         }
@@ -60,84 +58,82 @@ class HomeFragment : Fragment() {
             .addOnSuccessListener { document ->
                 val userDocument = document.documents[0]
                 userRights = userDocument.getString("rights").toString()
-                val collection = mFirestore.collection("Article")
-                // Tri par ordre décroissant des IDs
-                collection
-                    .orderBy("date", Query.Direction.DESCENDING).limit(8)
-                    .get()
-                    .addOnSuccessListener { querySnapshot ->
-                        val articles = ArrayList<Article>()
-                        for (document in querySnapshot) {
-                            val id = document.id.toString()
-                            Article.getArticle(id) { article ->
-                                if (article != null) {
-                                    articles.add(article)
-                                }
-                                // Vérifie si tous les articles ont été récupérés
-                                if (articles.size == querySnapshot.documents.size) {
-                                    articles.sortByDescending { it.date } // Trie les articles par ordre décroissant de la date
+            }
+        val collection = mFirestore.collection("Article")
+        // Tri par ordre décroissant des IDs
+        collection
+            .orderBy("date", Query.Direction.DESCENDING).limit(8)
+            .get()
+            .addOnSuccessListener { querySnapshot ->
+                val articles = ArrayList<Article>()
+                for (document in querySnapshot) {
+                    val id = document.id.toString()
+                    Article.getArticle(id) { article ->
+                        if (article != null) {
+                            articles.add(article)
+                        }
+                        // Vérifie si tous les articles ont été récupérés
+                        if (articles.size == querySnapshot.documents.size) {
+                            articles.sortByDescending { it.date } // Trie les articles par ordre décroissant de la date
 
-                                    for (article in articles) {
-                                        val articleVerified = article.verified.toString()
 
-                                        if (article != null && (articleVerified != "no" || (articleVerified == "no" && userRights != "0"))) {
+                            for (article in articles) {
 
-                                            if (!isAdded || context == null) {
-                                                return@getArticle  // Vérifier si le fragment est attaché et si le contexte est disponible
+                                val articleVerified = article.verified.toString()
+
+                                if (article != null && (articleVerified != "no" || (articleVerified == "no" && userRights != "0"))) {
+
+                                    val inflater = LayoutInflater.from(requireContext())
+                                    val postView = inflater.inflate(R.layout.post, null)
+                                    val cardView: CardView =
+                                        postView.findViewById(R.id.touchCard)
+                                    linearContainer.addView(postView)
+                                    val flagImageView =
+                                        postView.findViewById<ImageView>(R.id.redFlag)
+                                    if (userRights == "0" || articleVerified == "yes") {
+                                        flagImageView.visibility = View.INVISIBLE
+                                    }
+
+                                    val args = Bundle()
+
+                                    if (article.mediasID != "") {
+                                        val medias =
+                                            FirebaseFirestore.getInstance().collection("Medias")
+                                                .document(article.id)
+                                        medias.get().addOnSuccessListener { documentSnapshot ->
+                                            val media1 = documentSnapshot.getString("media1")
+                                            if (media1 != null) {
+                                                val mediaView: ImageView =
+                                                    postView.findViewById(R.id.imageView)
+                                                mediaView.layoutParams = LinearLayout.LayoutParams(
+                                                    LinearLayout.LayoutParams.MATCH_PARENT,
+                                                    300
+                                                )
+                                                Glide.with(requireContext())
+                                                    .load(media1)
+                                                    .into(mediaView)
                                             }
-
-                                            val inflater = LayoutInflater.from(requireContext())
-                                            val postView = inflater.inflate(R.layout.post, null)
-                                            val cardView: CardView =
-                                                postView.findViewById(R.id.touchCard)
-                                            linearContainer.addView(postView)
-                                            val flagImageView =
-                                                postView.findViewById<ImageView>(R.id.redFlag)
-                                            if (userRights == "0" || articleVerified == "yes") {
-                                                flagImageView.visibility = View.INVISIBLE
-                                            }
-
-                                            val args = Bundle()
-
-                                            if (article.mediasID != "") {
-                                                val medias =
-                                                    FirebaseFirestore.getInstance().collection("Medias")
-                                                        .document(article.id)
-                                                medias.get().addOnSuccessListener { documentSnapshot ->
-                                                    val media1 = documentSnapshot.getString("media1")
-                                                    if (media1 != null) {
-                                                        val mediaView: ImageView =
-                                                            postView.findViewById(R.id.imageView)
-                                                        mediaView.layoutParams = LinearLayout.LayoutParams(
-                                                            LinearLayout.LayoutParams.MATCH_PARENT,
-                                                            300
-                                                        )
-                                                        Glide.with(requireContext())
-                                                            .load(media1)
-                                                            .into(mediaView)
-                                                    }
-                                                }
-                                            }
-
-                                            val textView: TextView = postView.findViewById(R.id.textView)
-                                            textView.text = article.title
-
-                                            cardView.setOnClickListener {
-                                                args.putString("index", article.id)
-                                                val readFragment = ReadFragment()
-                                                readFragment.arguments = args
-                                                handleClick(readFragment, args)
-                                            }
-                                            currentId = articles.lastOrNull()?.id?.toInt() ?: 0
                                         }
                                     }
 
+                                    val textView: TextView = postView.findViewById(R.id.textView)
+                                    textView.text = article.title
+
+                                    cardView.setOnClickListener {
+                                        args.putString("index", article.id)
+                                        val readFragment = ReadFragment()
+                                        readFragment.arguments = args
+                                        handleClick(readFragment, args)
+                                    }
+                                    currentId = articles.lastOrNull()?.id?.toInt() ?: 0
                                 }
                             }
+
                         }
                     }
-                    .addOnFailureListener { exception -> }
+                }
             }
+            .addOnFailureListener { exception ->}
     }
 
     private fun createnextPost(view: View) {
@@ -156,7 +152,7 @@ class HomeFragment : Fragment() {
                 for (document in querySnapshot) {
                     val id = document.id.toString()
                     Article.getArticle(id) { article ->
-                        val articleVerified = article?.verified.toString()
+                        val articleVerified=article?.verified.toString()
                         if (article != null) {
                             articles.add(article)
                         }
@@ -166,8 +162,7 @@ class HomeFragment : Fragment() {
                             articles.sortByDescending { it.date } // Trie les articles par ordre décroissant de la date
                             val smallestId = articles.lastOrNull()?.id?.toInt() ?: 0
                             val idLimit = currentId - 5
-                            val articlesToDisplay =
-                                articles.filter { it.id.toInt() < currentId && it.id.toInt() > idLimit }
+                            val articlesToDisplay = articles.filter { it.id.toInt() < currentId && it.id.toInt() > idLimit }
                             for (article in articlesToDisplay) {
                                 val inflater = LayoutInflater.from(requireContext())
                                 val postView = inflater.inflate(R.layout.post, null)
@@ -181,14 +176,11 @@ class HomeFragment : Fragment() {
                                 val args = Bundle()
 
                                 if (article.mediasID != "") {
-                                    val medias =
-                                        FirebaseFirestore.getInstance().collection("Medias")
-                                            .document(article.id)
+                                    val medias = FirebaseFirestore.getInstance().collection("Medias").document(article.id)
                                     medias.get().addOnSuccessListener { documentSnapshot ->
                                         val media1 = documentSnapshot.getString("media1")
                                         if (media1 != null) {
-                                            val mediaView: ImageView =
-                                                postView.findViewById(R.id.imageView)
+                                            val mediaView: ImageView = postView.findViewById(R.id.imageView)
                                             mediaView.layoutParams = LinearLayout.LayoutParams(
                                                 LinearLayout.LayoutParams.MATCH_PARENT,
                                                 300
@@ -215,8 +207,38 @@ class HomeFragment : Fragment() {
                     }
                 }
             }
-            .addOnFailureListener { exception -> }
+            .addOnFailureListener { exception ->
+                //
+            }
     }
+
+
+
+
+
+    private fun detectEndOfScroll(scrollView: ScrollView) {
+        scrollView.viewTreeObserver.addOnScrollChangedListener {
+            val view = scrollView.getChildAt(scrollView.childCount - 1)
+            val diff = (view.bottom - (scrollView.height + scrollView.scrollY))
+            if (diff == 0) {
+                //changer la fonction create post par la fonction add other posts qui ajoute les posts des id suivants
+                createnextPost(view)
+            }
+        }
+    }
+
+    private fun refreshScroll(scrollView: ScrollView) {
+        scrollView.viewTreeObserver.addOnScrollChangedListener {
+            if (scrollView.scrollY == 0) {
+                val linearContainer: LinearLayout = scrollView.findViewById(R.id.fil)
+                linearContainer.removeAllViews()
+                currentId=0
+                createPost(scrollView.getChildAt(0))
+
+            }
+        }
+    }
+
     fun handleClick(fragment: Fragment, arguments: Bundle) {
         fragment.arguments = arguments
         (activity as HomeActivity).moveToFragment(fragment)
